bd9c7cfcc932702cd86b3c47c6cae2b5b47ff643
agus-mb <munosagustina3@gmail.com>
2023-06-08 10:43:37 -0300

    revisar JS, error al deslizar

--- index.js
+++ index.js
@@ -8,8 +8,29 @@ btnLeft.addEventListener("click", (e)=> moveToLeft())
 btnRight.addEventListener("click", (e)=>moveToRight())
 
 let operacion =0;
+contadorImg=0;
+widthImage= 100/ sliderSection.length;
 
 function moveToRight(){
-    operacion=operacion+33.3;
+    if(contadorImg >= sliderSection.length-1){
+        contadorImg=0;
+        operacion=0;
+return
+    }
+    contadorImg++;
+    operacion=operacion+ widthImage;
+    carrusel.style.transform= `translate(-${operacion}%)`
+}
+
+function moveToLeft(){
+    contadorImg--;
+    if(contadorImg<0){
+        contadorImg=sliderSection.length-1;
+        operacion=widthImage*(sliderSection.length-1)
+        console.log(operacion)
+        carrusel.style.transform= `translate(-${operacion}%)`
+
+    }
+    operacion=operacion - widthImage;
     carrusel.style.transform= `translate(-${operacion}%)`
 }
