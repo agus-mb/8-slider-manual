28b676fc4b16c645ab1f7eea2e41842e6a8f0478
agus-mb <munosagustina3@gmail.com>
2023-10-13 00:20:17 -0300

    prettier

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-(function(){
-    const sliders = [...document.querySelectorAll('.slider-body')];
-    const arrowNext= document.querySelector('#after');
-    const arrowBefore= document.querySelector('#before');
-    let value;
+(function () {
+  const sliders = [...document.querySelectorAll(".slider-body")];
+  const arrowNext = document.querySelector("#after");
+  const arrowBefore = document.querySelector("#before");
+  let value;
 
+  arrowNext.addEventListener("click", () => changePosition(1));
+  arrowBefore.addEventListener("click", () => changePosition(-1));
 
-    arrowNext.addEventListener('click', ()=>changePosition(1));
-    arrowBefore.addEventListener('click', ()=>changePosition(-1));
+  function changePosition(change) {
+    const currentElement = Number(
+      document.querySelector(".slider-body--show").dataset.id
+    );
 
-    function changePosition(change){
-        const currentElement= Number(document.querySelector('.slider-body--show').dataset.id);
+    value = currentElement;
+    value += change;
 
-        value=currentElement;
-        value +=change;
-
-if(value===0|| value==sliders.length+1){
-            value=value===0?sliders.length:1;
-        }
-
-        sliders[currentElement-1].classList.toggle('slider-body--show');
-        sliders[value-1].classList.toggle('slider-body--show');
-
+    if (value === 0 || value == sliders.length + 1) {
+      value = value === 0 ? sliders.length : 1;
     }
-})()
+
+    sliders[currentElement - 1].classList.toggle("slider-body--show");
+    sliders[value - 1].classList.toggle("slider-body--show");
+  }
+})();
